7b0b501b00c18072db6c3735f0cbaf9f35defbfd
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-03 14:24:17 +0300

    for statement

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,11 @@
 from flask import render_template,url_for
 from . import main
+from ..requests import get_sources
 
 
 @main.route("/")
 def index():
     title="Sources"
+    all_news=get_sources()
 
-    return render_template("index.html", title=title)
+    return render_template("index.html", title=title, news=all_news)
