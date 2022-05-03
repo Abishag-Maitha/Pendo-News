bcdb2465198bc3d95cd6230f2c7ffff3510ac1e1
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-03 12:22:06 +0300

    create source class

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
+from nturl2path import url2pathname
+from typing_extensions import Self
+
+
 class Article:
     def __init__(self, source, author,title, description, url, urlToImage, publishedAt, content):
         self.source=source
@@ -8,4 +12,15 @@ class Article:
         self.urlToImage=urlToImage
         self.publishedAt=publishedAt
         self.content=content
+
+class Source:
+    def __init__(self, id, name, description, url, category, language, country):
+        self.id=id
+        self.name=name
+        self.url=url
+        self.category=category
+        self.language=language
+        self.country=country
+
+    
         
