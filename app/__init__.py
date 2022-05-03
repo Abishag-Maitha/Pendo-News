db378e66a141422bc8f713cec782f45cde68f7b7
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-05-03 12:06:55 +0300

    register blue print

--- app/__init__.py
+++ app/__init__.py
@@ -8,5 +8,8 @@ def create_app(config_name):
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
-    
+    #Registering a blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
